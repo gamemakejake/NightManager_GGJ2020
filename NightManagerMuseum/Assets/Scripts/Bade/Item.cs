c6db4fbb0df032737d65c57469c5fd71c4d39544
JungDefiant <42555252+JungDefiant@users.noreply.github.com>
2020-02-01 17:47:15 -0800

    Lots Of Things

---
+++ NightManagerMuseum/Assets/Scripts/Bade/Item.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "NightManagerMuseum/Item", order = 0)]
+public class Item : ScriptableObject {
+    public Sprite sprite;
+}
