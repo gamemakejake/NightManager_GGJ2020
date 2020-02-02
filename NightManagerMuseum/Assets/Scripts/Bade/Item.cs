4f17bb5d4a26dc7ce1675255f05fb4a1395d829d
GameMakeJake <jacobshillan@gmail.com>
2020-02-01 17:59:21 -0800

    jake push

--- NightManagerMuseum/Assets/Scripts/Bade/Item.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item", menuName = "NightManagerMuseum/Item", order = 0)]
-public class Item : ScriptableObject {
-    public Sprite sprite;
-}
